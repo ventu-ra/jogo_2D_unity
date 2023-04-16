6db4a47a1357f40242b06801a29d105b07f7acc7
ventu-ra <ventura09.10.0@gmail.com>
2023-04-16 18:22:15 -0300

    organizando os arquivos por pasta end C_controller

--- Assets/script/PlayerMovement.cs
+++ Assets/script/PlayerMovement.cs
@@ -9,6 +9,8 @@ public class PlayerMovement : MonoBehaviour
   private Animator anim;
 
   private float dirX = 0f;
+  [SerializeField] private float moveSpeed = 7f;
+  [SerializeField] private float jumpForce = 14f;
 
   // Start is called before the first frame update
   private void Start()
@@ -23,17 +25,17 @@ public class PlayerMovement : MonoBehaviour
   {
 
     dirX = Input.GetAxisRaw("Horizontal");
-    rb.velocity = new Vector2(dirX * 7f, rb.velocity.y);
+    rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
 
     if (Input.GetButtonDown("Jump"))
     {
-      rb.velocity = new Vector2(rb.velocity.x, 14f);
+      rb.velocity = new Vector2(rb.velocity.x, jumpForce);
     }
 
-    UpdateAnimationUpdate();
+    UpdateAnimationState();
   }
 
-  private void UpdateAnimationUpdate()
+  private void UpdateAnimationState()
   {
     if (dirX > 0f)
     {
