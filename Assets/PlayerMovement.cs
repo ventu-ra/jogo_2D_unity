440cded4d7517481afe9bd9d90ca4abe75567a18
ventu-ra <ventura09.10.0@gmail.com>
2023-04-16 17:55:14 -0300

    add animation

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,17 +4,50 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+  private Rigidbody2D rb;
+  private SpriteRenderer sprite;
+  private Animator anim;
+
+  private float dirX = 0f;
+
   // Start is called before the first frame update
-  void Start()
+  private void Start()
   {
+    rb = GetComponent<Rigidbody2D>();
+    sprite = GetComponent<SpriteRenderer>();
+    anim = GetComponent<Animator>();
   }
 
   // Update is called once per frame
-  void Update()
+  private void Update()
   {
-    if (Input.GetKey("space"))
+
+    dirX = Input.GetAxisRaw("Horizontal");
+    rb.velocity = new Vector2(dirX * 7f, rb.velocity.y);
+
+    if (Input.GetButtonDown("Jump"))
     {
-        GetComponent<Rigidbody2D>().velocity = new Vector3(0, 14, 0);
+      rb.velocity = new Vector2(rb.velocity.x, 14f);
+    }
+
+    UpdateAnimationUpdate();
+  }
+
+  private void UpdateAnimationUpdate()
+  {
+    if (dirX > 0f)
+    {
+      anim.SetBool("running", true);
+      sprite.flipX = false;
+    }
+    else if (dirX < 0f)
+    {
+      anim.SetBool("running", true);
+      sprite.flipX = true;
+    }
+    else
+    {
+      anim.SetBool("running", false);
     }
   }
 }
